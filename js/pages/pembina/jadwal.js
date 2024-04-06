f4236dab90a404ee4a9873ec2d6ae6d743e98d14
Ali Hanafiah <hanafiaha308@gmail.com>
2024-04-06 10:18:42 +0700

    menambah kam pop up

---
+++ js/pages/pembina/jadwal.js
@@ -0,0 +1,5 @@
+document.querySelector("#button-tambah").onclick = () => document.querySelector("#tambah").style.display = "block";
+document.querySelector("#silang-1").onclick = () => document.querySelector("#tambah").style.display = "none";
+
+document.querySelector(".edit").onclick = () => document.querySelector("#edit-1").style.display = "block";
+document.querySelector("#silang-2").onclick = () => document.querySelector("#edit-1").style.display = "none";
