d238f402653b384ebbfb2fb92818fad97f4529b6
Ali Hanafiah <hanafiaha308@gmail.com>
2024-04-15 18:41:37 +0700

    aaaa

--- js/pages/pembina/jadwal.js
+++ js/pages/pembina/jadwal.js
@@ -31,8 +31,28 @@ document.querySelector("#button-tambah").onclick = async () => {
         // option.style.color = "-webkit-linear-gradient(315deg, #1e30f3 0%, #e21e80 100%)";
         document.querySelector("#jam").appendChild(option);
     });
+    const instruktur = await api("/get-instruktur");
+    instruktur.data.forEach(val => {
+        const option = document.createElement("option");
+        option.value = val.id;
+        option.innerHTML = val.nama;
+        option.style.textAlign = "center";
+        // option.style.color = "-webkit-linear-gradient(315deg, #1e30f3 0%, #e21e80 100%)";
+        document.querySelector("#instruktur").appendChild(option);
+    });
 };
-document.querySelector("#silang-1").onclick = () => document.querySelector("#tambah").style.display = "none";
 
+document.querySelector("#silang-1").onclick = () => document.querySelector("#tambah").style.display = "none";
 document.querySelector(".edit").onclick = () => document.querySelector("#edit-1").style.display = "block";
 document.querySelector("#silang-2").onclick = () => document.querySelector("#edit-1").style.display = "none";
+
+
+document.add_nilai.onsubmit = async (e) => {
+    e.preventDefault();
+    const data = await api("/add-pelatihan", "POST", {
+        name: document.add_nilai.name.value,
+        id_categori_pelatihan: document.categori_pelatihan.name.value,
+        jam: document.jam.value,
+        hari: document.hari.value
+    });
+}
