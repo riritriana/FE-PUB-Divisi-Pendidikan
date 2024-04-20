7d269e4b013adc4c3b3492616d7f8034a6501401
Ali Hanafiah <hanafiaha308@gmail.com>
2024-04-21 03:35:46 +0700

    membuat taable

--- js/pages/pembina/jadwal.js
+++ js/pages/pembina/jadwal.js
@@ -61,3 +61,90 @@ document.add_nilai.onsubmit = async (e) => {
     alert(data.msg);
 
 }
+
+
+const getJadwal = async () => {
+    const kumpulanData = await api("/get-jadwal-pembina");
+    kumpulanData.data.jadwal.forEach(data => {
+        console.log(data);
+        const table = document.createElement("table");
+        table.classList.add("table");
+        table.setAttribute("border", "1");
+        table.setAttribute("style", "background: -webkit-linear-gradient(315deg, #1e30f3 0%, #e21e80 100%); color: #fff;");
+
+        // Buat elemen thead
+        const thead = document.createElement("thead");
+        thead.classList.add("thead-dark");
+
+        // Buat baris pertama thead
+        const firstRow = document.createElement("tr");
+        const firstRowHeader = document.createElement("th");
+        firstRowHeader.setAttribute("scope", "col");
+        firstRowHeader.setAttribute("colspan", "6");
+        firstRowHeader.setAttribute("style", "vertical-align: middle; text-align: center;");
+        firstRowHeader.textContent = `Pelatihan ${data.categori_pelatihan.name} (${data.name}) `;
+        firstRow.appendChild(firstRowHeader);
+        thead.appendChild(firstRow);
+
+        // Buat baris kedua thead
+        const secondRow = document.createElement("tr");
+        const instrukturHeader = document.createElement("th");
+        instrukturHeader.setAttribute("colspan", "3");
+        instrukturHeader.setAttribute("style", "vertical-align: middle; text-align: center;");
+        instrukturHeader.textContent = "Instruktur " + data.categori_pelatihan.name;
+        secondRow.appendChild(instrukturHeader);
+        const anggotaHeader = document.createElement("th");
+        anggotaHeader.setAttribute("colspan", "3");
+        anggotaHeader.setAttribute("style", "vertical-align: middle; text-align: center;");
+        anggotaHeader.textContent = data.user.nama;
+        secondRow.appendChild(anggotaHeader);
+        thead.appendChild(secondRow);
+
+        // Buat baris ketiga thead
+        var thirdRow = document.createElement("tr");
+        var anggotaTitle = document.createElement("th");
+        anggotaTitle.setAttribute("colspan", "3");
+        anggotaTitle.setAttribute("style", "vertical-align: middle; text-align: center;");
+        anggotaTitle.textContent = "Anggota";
+        thirdRow.appendChild(anggotaTitle);
+        var jadwalTitle = document.createElement("th");
+        jadwalTitle.setAttribute("colspan", "3");
+        jadwalTitle.setAttribute("style", "vertical-align: middle; text-align: center;");
+        jadwalTitle.textContent = "Jadwal Pelatihan";
+        thirdRow.appendChild(jadwalTitle);
+        thead.appendChild(thirdRow);
+
+        // Masukkan thead ke dalam tabel
+        table.appendChild(thead);
+        var tbody = document.createElement("tbody");
+
+        // Buat baris pertama data
+        data.kumpulan_anggota.forEach((anggota, index) => {
+            const tr = document.createElement("tr");
+            const td = document.createElement("td");
+            td.setAttribute("colspan", "3");
+            td.setAttribute("style", "vertical-align: middle; text-align: center;");
+            td.textContent = anggota.user.nama;
+            tr.appendChild(td);
+            if (index === 0) {
+                const td2 = document.createElement("td");
+                td2.setAttribute("rowspan", data.kumpulan_anggota.length);
+                td2.setAttribute("colspan", "3");
+                td2.setAttribute("style", "vertical-align: middle; text-align: center;");
+                td2.textContent = data.jadwal_pelatihan.hari + " / " + data.jadwal_pelatihan.jam;
+                tr.appendChild(td2);
+            }
+
+            tbody.appendChild(tr);
+
+        });
+
+
+        table.appendChild(tbody);
+        document.querySelector("#table").appendChild(table);
+
+    })
+
+}
+
+getJadwal();
